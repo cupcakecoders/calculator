1850265ab8eb35ff6ea566459cd78cd11893e24a
K Dilawari <cavey.waveyy@gmail.com>
2020-01-25 15:50:02 +0000

    add operator switch statement

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,13 +7,35 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Calculator!");
+            
+            Console.WriteLine("Enter an operator");
+            string myOperator = Console.ReadLine();
+
             Console.WriteLine("Type your first number");
             int firstNumber = int.Parse(Console.ReadLine());
             
             Console.WriteLine("Type your second number");
             int secondNumber = int.Parse(Console.ReadLine());
+
+            int answer = 0;
+            switch (myOperator)
             
-            Console.WriteLine(firstNumber * secondNumber);
+            {
+                case "*":
+                    answer = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    answer = firstNumber / secondNumber;
+                    break;
+                case "+":
+                    answer = firstNumber + secondNumber;
+                    break;
+                case "-":
+                    answer = firstNumber - secondNumber;
+                    break;
+            }
+            
+            Console.WriteLine($"{firstNumber} {myOperator} {secondNumber} = {answer}");
         }
     }
 }
