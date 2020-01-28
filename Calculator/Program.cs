815decf00e27cd9098a0e3138d678183e709d51f
K Dilawari <cavey.waveyy@gmail.com>
2020-01-28 21:14:25 +0000

    create class for perform one calculation

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 
+
 namespace Calculator
 {
     class Program
@@ -20,7 +21,8 @@ namespace Calculator
 
                 if (calculationMode == NumberCalculator)
                 {
-                    PerformOneCalculation();
+                    var newcalculation = new NumberCalculatorx();
+                    newcalculation.PerformOneCalculation();
                 }
                 else
                 {
@@ -32,59 +34,6 @@ namespace Calculator
         static void PrintWelcomeMessage()
         {
             Console.WriteLine("Welcome to the Calculator!");
-        }
-
-        static void PerformOneCalculation()
-        {
-            Console.WriteLine("Enter an operator * - + /");
-            string myOperator = Console.ReadLine();
-
-            var numberOfnumbers = UserInput(
-                $"How many numbers do you want to {myOperator} ?");
-
-            int[] numbersArray = new int[numberOfnumbers];
-            
-            int count = 1;
-
-            for (int i = 0; i < numbersArray.Length; i++)
-            {
-                int myNumber = UserInput($"Please enter number {count}");
-                count = count + 1;
-                numbersArray[i] = myNumber;
-            }
-
-            // calculator method to go through each number in array and apply operator to it
-            var answer = numbersArray[0];
-            for (int i = 0; i < numbersArray.Length -1; i++)
-            {
-                Console.WriteLine($"The number we are dealing with {numbersArray[i + 1]}");
-                Console.WriteLine($"The answer currently is {answer}");
-
-                switch (myOperator)
-
-                {
-                    case "*":
-                        answer = answer * numbersArray[i + 1];
-                        break;
-                    case "/":
-                        answer = answer / numbersArray[i + 1];
-                        break;
-                    case "+":
-                        answer = answer + numbersArray[i + 1];
-                        break;
-                    case "-":
-                        answer = answer - numbersArray[i + 1];
-                        break;
-                }
-            }
-            Console.WriteLine($"The answer is {answer}");
-        }
-
-        static int UserInput(string prompt)
-        {
-            Console.WriteLine(prompt);
-            var number = GetNumber(Console.ReadLine());
-            return number;
         }
 
         static DateTime UserInputDate(string prompt)
@@ -105,7 +54,13 @@ namespace Calculator
                 return UserInputDate("That was not a valid date, please enter a date mm/dd/yyyy");
             }
         }
-
+        static int UserInput(string prompt)
+        {
+            Console.WriteLine(prompt);
+            var number = GetNumber(Console.ReadLine());
+            return number;
+        }
+        
         static int GetNumber(string response)
         {
             if (int.TryParse(response, out int number))
